c9df8a1264af7e117292677230f96be1149714f9
MagusWyvern <72914828+MagusWyvern@users.noreply.github.com>
2022-12-15 08:44:55 +0000

    Ping Pong Game Upload

---
+++ ping-pong-qas/Level/Level.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var PlayerScore = 0
+var OpponentScore = 0
+
+func _on_Left_body_entered(body):
+	score_achieved()
+	OpponentScore += 1
+
+func _on_Right_body_entered(body):
+	score_achieved()
+	PlayerScore += 1
+
+func _process(delta):
+	$PlayerScore.text = str(PlayerScore)
+	$OpponentScore.text = str(OpponentScore)
+	$CountdownLabel.text = str(int($CountdownTimer.time_left) + 1)
+
+func _on_CountdownTimer_timeout():
+	get_tree().call_group('BallGroup','restart_ball')
+	$CountdownLabel.visible = false
+
+func score_achieved():
+	$Ball.position = Vector2(640,360)
+	
+	get_tree().call_group('BallGroup','stop_ball')
+	$CountdownTimer.start()
+	$CountdownLabel.visible = true
+	$ScoreSound.play()
+	$Player.position.x = 35
+	$Opponent.position.x = 1280 - 35
